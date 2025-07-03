97677275d45ba708420c5753e3dfd3ffa1ca8567
Schluecu <136748981+Schluecu@users.noreply.github.com>
2025-07-03 21:06:19 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function openDesc1() {
-  var x = document.getElementById("desc1");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
