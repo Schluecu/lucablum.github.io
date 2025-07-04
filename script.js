9cd43413b3ca7421ff6d13334769fd9827236273
Schluecu <136748981+Schluecu@users.noreply.github.com>
2025-07-05 01:10:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,10 +7,9 @@ function myFunction() {
     y.style.display="inline-block";
     z.style="font-size:5vw; color: black;-webkit-text-stroke-color: black;-webkit-text-stroke-width: 0;";
   } else {
-    x.style.float = "left";
     y.style.display="none";
     z.style="font-size:5vw; color: transparent;-webkit-text-stroke-color: black;-webkit-text-stroke-width: calc(3.5vmax/50);";
-    x.style="display:block; color: transparent;-webkit-text-stroke-color: black;-webkit-text-stroke-width: calc(3.5vmax/50);";
+    x.style="width:20vw;padding-top:5vw;display:block; float:left; color: transparent;-webkit-text-stroke-color: black;-webkit-text-stroke-width: calc(3.5vmax/50);";
   }
 }
 function openDesc1() {var x = document.getElementById("desc1");if (x.style.display === "none") {x.style.display = "block";} else {x.style.display = "none";}}
