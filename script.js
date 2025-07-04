5a735e2415a2d23b0a7e6ee772b46c8ef0af5eae
Schluecu <136748981+Schluecu@users.noreply.github.com>
2025-07-05 01:02:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ function myFunction() {
   if (x.style.display === "block") {
     x.style.display = "none";
     y.style.display="inline-block";
-    z.style="color: black;-webkit-text-stroke-width: 0;font-size:5vw;"
+    z.style="color: black;-webkit-text-stroke-width: 0;font-size:5vw;";
   } else {
     x.style.display = "block";
     x.style.float = "left";
     y.style.display="none";
-    z.style="color: transparent;-webkit-text-stroke-color: black;-webkit-text-stroke-width: calc(3.5vmax/50);"
-    x.style="color: transparent;-webkit-text-stroke-color: black;-webkit-text-stroke-width: calc(3.5vmax/50);"
+    z.style="font-size:5vw; color: transparent;-webkit-text-stroke-color: black;-webkit-text-stroke-width: calc(3.5vmax/50);";
+    x.style="color: transparent;-webkit-text-stroke-color: black;-webkit-text-stroke-width: calc(3.5vmax/50);";
   }
 }
 function openDesc1() {var x = document.getElementById("desc1");if (x.style.display === "none") {x.style.display = "block";} else {x.style.display = "none";}}
