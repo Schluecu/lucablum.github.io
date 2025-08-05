5e48291f93e4b0bdd096620ef2900aef1db5ea04
Schluecu <136748981+Schluecu@users.noreply.github.com>
2025-08-05 22:10:38 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,8 +12,4 @@ function myFunction() {
     x.style="width:20vw;padding-top:5vw;display:block; float:left; color: transparent;-webkit-text-stroke-width: calc(3.5vmax/50);";
   }
 }
-function openDesc1() {var x = document.getElementById("desc1");if (x.style.display === "none") {x.style.display = "block";} else {x.style.display = "none";}}
-function openDesc2() {var x = document.getElementById("desc2");if (x.style.display === "none") {x.style.display = "block";} else {x.style.display = "none";}}
-function openDesc3() {var x = document.getElementById("desc3");if (x.style.display === "none") {x.style.display = "block";} else {x.style.display = "none";}}
-function openDesc4() {var x = document.getElementById("desc4");if (x.style.display === "none") {x.style.display = "block";} else {x.style.display = "none";}}
-  
+function openDesc(id) {var x = document.getElementById(id);if (x.style.display === "none") {x.style.display = "block";} else {x.style.display = "none";}}
