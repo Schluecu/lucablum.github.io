29b3e9ffa5121400b7eea46b6509eae97d1bb47e
Schluecu <136748981+Schluecu@users.noreply.github.com>
2025-08-16 01:27:54 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -38,8 +38,7 @@ function filti(element) {
     selectors[i].style.fontFamily="ABC Otto Variable Edu Regular";
     stil.style.fontFamily="ABC Otto Variable Edu Light Italic";
     lemenent(lmnt);
-    console.log(selecting.indexOf(element));
-    active[selecting.indexOf(element)]++;
+    active[selecting.indexOf(element)]=1;
     }
     }
 }}
