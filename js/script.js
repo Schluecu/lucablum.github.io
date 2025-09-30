7f863365f5affe55ceddfba0f93b08716812f9d4
Schluecu <136748981+Schluecu@users.noreply.github.com>
2025-09-30 12:38:44 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ function openDesc(theid) {
 
 function showReference(theid) {
   var y = document.getElementById(theid+'trig')
-  if (y.class === 'unopened') {y.class = "opened"}
+  if (y.class === "unopened") {y.class = "opened"}
   var x = document.getElementById(theid);
   if (x.style.display === "none") {x.style.display = "inline";}
   else {x.style.display = "none";}}
