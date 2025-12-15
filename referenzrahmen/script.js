de92d8aafe46f274de73d59cd2178676ad808165
Schluecu <136748981+Schluecu@users.noreply.github.com>
2025-12-15 23:17:35 +0100

    Update script.js

--- referenzrahmen/script.js
+++ referenzrahmen/script.js
@@ -1,10 +1,15 @@
 function unlock(unlock) {
-    document.getElementById(unlock).style.display = "inline";
+    if(document.getElementById(unlock).style.display=="inline"){
+    document.getElementById(unlock+'_').style.display="inline"}else{
+    document.getElementById(unlock).style.display = "inline";}
 }
 
 function load(load) {
+    if(document.getElementById(load).classList.contains=="done"){
+    document.getElementById(load+'_').classList.add("on");
+    document.getElementById(load+'_').classList.remove("off");}else{
     document.getElementById(load).classList.add("on");
-    document.getElementById(load).classList.remove("off");
+    document.getElementById(load).classList.remove("off");}
 }
 
 function lock(lock) {
