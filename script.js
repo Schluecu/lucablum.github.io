468f21dc44e5e82c98c233cb5ee7e8424d462993
Schluecu <136748981+Schluecu@users.noreply.github.com>
2026-01-25 15:33:05 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function impressum() {
+    if (document.getElementById('desc').style.display=="none"){
+        document.getElementById('desc').style.display="flex";
+    }   else {
+        document.getElementById('desc').style.display="none";
+    }
+}
